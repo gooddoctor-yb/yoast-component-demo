eaec3c918843c8f9ad4be5610f29e3171b0a5723
yibu.wang <yibu.wang@gooddoctor.com.sg>
2020-11-01 12:19:54 +0800

    add server

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -8,13 +8,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
-          <div className="flex-shrink-0">
-            <input />
-            test
-            {/* <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" /> */}
-          </div>
           <div className="ml-6 pt-1">
-            <h4 className="text-xl text-gray-900 leading-tight">NIHAO </h4>
+            <h4 className="text-xl text-gray-900 leading-tight">hello</h4>
             <p className="text-base text-gray-600 leading-normal">You have a new message!</p>
           </div>
         </div>
--- src/components/demo.js
+++ src/components/demo.js
@@ -1,5 +1,3 @@
-import { hot } from "react-hot-loader";
-
 import React, { Component } from "react";
 
 export default class Demo extends Component {
